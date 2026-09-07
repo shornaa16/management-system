792af81ffcafcae174115d6c89cf52a908be3412
shorna <naharshorna16@gmail.com>
2026-09-07 04:23:31 -0400

    Fix Render environment setup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,12 +5,16 @@ cd /var/www/html
 
 mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views bootstrap/cache
 
-if [ ! -f database/database.sqlite ]; then
-    touch database/database.sqlite
+if [ ! -f .env ]; then
+    touch .env
 fi
 
 if [ -z "${APP_KEY:-}" ]; then
     php artisan key:generate --force --no-interaction
+fi
+
+if [ ! -f database/database.sqlite ]; then
+    touch database/database.sqlite
 fi
 
 php artisan config:clear --no-interaction
